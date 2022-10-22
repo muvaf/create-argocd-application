97958517054d93e08da23e6c120cb0b6ef00ecfa
Muvaffak Onus <me@muvaf.com>
2022-10-23 00:25:51 +0300

    input: add input fields
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

--- src/create.ts
+++ src/create.ts
@@ -1,23 +1,39 @@
 import { createTemplateAction } from '@backstage/plugin-scaffolder-backend';
 import fs from 'fs-extra';
 
-export const createNewFileAction = () => {
+export const createArgoCDHelmApplicationAction = () => {
     return createTemplateAction<{ contents: string; filename: string }>({
-        id: 'mycompany:create-file',
+        id: 'argocd:create-helm-application',
         schema: {
             input: {
-                required: ['contents', 'filename'],
                 type: 'object',
+                required: ['name', 'chart'],
                 properties: {
-                    contents: {
+                    name: {
                         type: 'string',
-                        title: 'Contents',
-                        description: 'The contents of the file',
+                        title: 'Name',
+                        description: 'The name of the Application',
                     },
-                    filename: {
-                        type: 'string',
-                        title: 'Filename',
-                        description: 'The filename of the file that will be created',
+                    chart: {
+                        type: 'object',
+                        required: ['repo', 'name', 'version'],
+                        properties: {
+                            repo: {
+                                type: 'string',
+                                title: 'Repository',
+                                description: 'The repository address, i.e. ghcr.io/organization',
+                            },
+                            name: {
+                                type: 'string',
+                                title: 'Name',
+                                description: 'The name of the chart, i.e. mychart',
+                            },
+                            version: {
+                                type: 'string',
+                                title: 'Version',
+                                description: 'The version of the chart, i.e. 0.1.0',
+                            },
+                        }
                     },
                 },
             },
@@ -30,3 +46,25 @@ export const createNewFileAction = () => {
         },
     });
 };
+
+// apiVersion: argoproj.io/v1alpha1
+// kind: Application
+// metadata:
+//     name: muvaf-kubecon-testing
+// spec:
+//     destination:
+//         name: ''
+// namespace: muvaf-kubecon-testing
+// server: 'https://kubernetes.default.svc'
+// source:
+//     path: ''
+// repoURL: ghcr.io/muvaf
+// targetRevision: 0.1.0
+// chart: muvaf-kubecon-testing-chart
+// project: default
+// syncPolicy:
+//     automated:
+//         prune: false
+// selfHeal: false
+// syncOptions:
+//     - CreateNamespace=true
